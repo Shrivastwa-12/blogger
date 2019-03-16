00c9a49300f3d05f92c3147aca49ebe9931c0fd3
Shrivastwa-12 <aakash1sep@gmail.com>
2019-03-16 18:35:02 +0530

    initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,130 @@
+<html>
+  <head>
+     <title>contact us
+	 </title>
+           <style>
+		   *{
+			  margin:0px;
+			  padding:0px;
+			  *}
+           .abc
+		   {
+			  height:600px;
+			  width:100%;
+			  }
+			  .div1
+			  {
+			   background-color:pink;
+			  height:120px;
+			  width:100%;
+			  font-size:30px;
+			  
+		  background-image:url("aaa.jpg");
+		      background-repeat:no-repeat;
+		      background-position:left;
+			  }
+			  .div2
+			  {
+			   background-color:orange;
+			  height:550px;
+			  width:40%;
+			  font-size:20px;
+			  float:left;
+			  
+			  
+			  }
+			  .div3
+			  {
+			   background-color:white;
+			  height:550px;
+			  width:60%;
+			  float:right;
+			
+			  }
+			 
+			  .div5{
+			  font-size:20px;
+			  }
+			  .div6
+			  {
+			   background-color:black;
+			  height:150px;
+			  width:100%;
+			  font-size:30px;
+			  color:White;
+			  }
+ 			  .menu 
+			  {
+			   background-color:violet;
+			   height:50px;
+			   width:100%;
+			    background-image:url("i.jpg");
+		      background-repeat:no-repeat;
+		      background-position:right;
+				
+				
+			   }
+			   
+			    ul li{ 
+			   display:inline;
+			   margin-left:70px;
+			   font-size:20px;
+			   text-decoration=non;
+			   }
+			   iframe{
+			   width:100%;
+			   }
+			  
+			 // font-size:30px;
+			//  color:White;
+			 
+			  </style>
+    </head>
+	  <body>
+	    <div class="abc">
+		    <div class="div1">
+			<p align="center" style="padding-top:30px;"><b>CONTACT US</b></p>
+
+			</div>
+			<div class="menu">
+			<ul style="list-style-type:disc">
+			 <li><a href="/">Home</li>
+			 <li align="center"><b><a href="/about">About us</li>
+			 <li><b><a href="/contact">contact</li>
+			 
+			 </ul>
+			</div>
+			 <div class="div2">
+			<form method='post'>
+			<label for="name">Name</label>
+			<input type="text" name="name"placeholder="Enter your name" size="40"><br><br>
+			
+			 <label for="Address">Address</label>&nbsp;&nbsp;&nbsp;
+			<textarea name="Address"placeholder="Enter your Address" rows="3" cols="15" size="10"></textarea><br>
+			 
+			 <label for="Phone No.">Phone No</label>
+			<input type="number" name="name"placeholder="Enter your Phone No" size="20"><br>
+			</form>
+			</div>
+			 <div class="div3">
+			 <h1>World Map</h1>
+			 <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.0006565610497!2d77.25945751460479!3d28.53969998245455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3de132b4909%3A0xb23d21cd15c91719!2sIndev+Consultancy+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1546866356506" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			 </div>
+			  <div class="div4">
+			  
+			 </div>
+			 <div class="div5">
+			 
+			
+			 </div>
+			
+		
+		</div>
+		<div class="div6">
+		<p align="center" style="padding-top: 60px;">This is Footer</p>
+			</div>
+		</body>
+		</html>
+		
+          		 
+	 
---
+++ resources/views/index.blade.php
@@ -0,0 +1,143 @@
+<html>
+  <head>
+     <title>Index
+	 </title>
+           <style>
+		   *{
+			  margin:0px;
+			  padding:0px;
+			  *}
+           .abc
+		   {
+			  height:600px;
+			  width:100%;
+			  }
+			  .div1
+			  {
+			   background-color: #ff6699;
+			  height:120px;
+			  width:100%;
+			  font-size:30px;
+			  
+		  background-image:url("aaa.jpg");
+		      background-repeat:no-repeat;
+		      background-position:left;
+			  }
+			  .div2
+			  {
+			   background-color: #c44dff;
+			  height:550px;
+			  width:20%;
+			  font-size:20px;
+			  float:left;
+			  margin-top=10px;
+			  
+			  
+			  }
+			  .div3
+			  {
+			   background-color: #ff8533;
+			  height:550px;
+			  width:80%;
+			  float:right;
+			
+			  }
+			  .div4
+			  {
+			   background-color:red;
+			  height:550px;
+			  width:50%;
+			  float:left;
+			  }
+			  .div5{
+			  font-size:20px;
+			  }
+			  .div6
+			  {
+			   background-color:black;
+			  height:150px;
+			  width:100%;
+			  font-size:30px;
+			  color:White;
+			  }
+ 			  .menu 
+			  {
+			   background-color:pink;
+			   height:50px;
+			   width:100%;
+			    background-image:url("i.jpg");
+		      background-repeat:no-repeat;
+		      background-position:right;
+			  line-height:2.5em;
+				
+				
+			   }
+			   
+			    ul li{ 
+			   display:inline;
+			   margin-left:70px;
+			   font-size:20px;
+			 
+			   
+			   }
+			   .menu a
+			   {
+			   text-decoration: none;
+			   
+			   }
+			 // font-size:30px;
+			//  color:White;
+			 
+			  </style>
+    </head>
+	  <body>
+	    <div class="abc">
+		    <div class="div1">
+			<p align="center" style="padding-top:30px;"><b>World INFORMTION</b></p>
+
+			</div>
+			<div class="menu">
+			<ul style="list-style-type:disc">
+			 <li><a href="/">Home</li>
+			 <li align="center"><b><a href="/about">About us</li>
+			 <li><b><a href="/contact">contact</li>
+			 
+			 </ul>
+			</div>
+			 <div class="div2">
+			 <p>LIST OF COUNTRY IN THE WORLD</p>
+			 <ol style="list-style-type:square">
+			 <LI><b><a href="" target='_blank'>INDIA</b></a></li>
+			 <li><b><a href="">CHINA</b></a></li>
+			 <li><b><a href="">NEPAL</li></b></a>
+			 <ol>
+
+			 
+			</div>
+			 <div class="div3">
+			  <div class="div4">
+			  <marquee behavior="alternate"><img src="{{ url('/') }}/image/ak1.jpg" alt="my pic" width="400" height="400"></marquee>
+			  
+			 </div>
+			 <div class="div5">
+			 <p align="justify">The world is the planet Earth and all life upon it, including human civilization.[1] In a philosophical context, the world is the whole of the physical Universe, or an ontological world. In a theological context, the world is the material or the profane sphere, as opposed to the celestial, spiritual, transcendent or sacred. The "end of the world" refers to scenarios of the final end of human history, often in religious contexts.
+
+World history is commonly understood as spanning the major geopolitical developments of about five millennia, from the first civilizations to the present. In terms such as world religion, world language, world government, and world war, world suggests international or intercontinental scope without necessarily implying participation of the entire world.
+
+World population is the sum of all human populations at any time;</p>
+<object data="horse.wav">
+<param name="autoplay" value="true">
+</object
+			
+			 </div>
+			
+		</div>
+		</div>
+		<div class="div6">
+		<p align="center" style="padding-top: 60px;">This is Footer</p>
+			</div>
+		</body>
+		</html>
+		
+          		 
+	 
